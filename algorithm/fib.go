7bfca0b476bdc17f99da89d7f65675e5d52a0812
shanhai3000 <shanhai3000@gmail.com>
2020-04-22 00:46:47 +0800

    bugfix

--- algorithm/fib.go
+++ algorithm/fib.go
@@ -5,7 +5,7 @@ let's say fib sequence is started with 0, 1, 1, 2, 3, 5, 8, ... and so on
 out : 44.692877843s (45.7265 in c++11)
 */
 func Fib(n int) uint64{
-	if n == 1{
+	if n <= 1{
 		return 0
 	}else if n == 2 || n == 3{
 		return 1
