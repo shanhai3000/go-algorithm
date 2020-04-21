16a5545dad5c060c26e672944f5b7bef5d01353e
shanhai3000 <shanhai3000@gmail.com>
2020-04-22 01:16:56 +0800

    add Stack test

--- test/stack_test.go
+++ test/stack_test.go
@@ -1,1 +1,31 @@
 package test
+
+import (
+	"algo/collection"
+	"testing"
+)
+
+func TestStack(t *testing.T){
+	s := collection.Stack{}
+	s.Push(1)
+
+	if s.Empty() {
+		t.Error("Empty() Error")
+	}else{
+		t.Log("Empty() Ok", s.Empty())
+	}
+	if s.Size() != 1{
+		t.Error("Size() Error")
+	}else{
+		t.Log("Size() Ok", s.Size())
+	}
+
+	i := s.Pop()
+	if i != 1 {
+		t.Error("Pop() Error")
+	}else{
+		t.Log("Pop() Ok", i)
+	}
+
+
+}
