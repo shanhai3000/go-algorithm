307a651f1c96dda81f2d4dddc4917d56c7975868
shanhai3000 <shanhai3000@gmail.com>
2020-04-23 14:15:50 +0800

    queue test

---
+++ test/queue_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"algo/collection/queue"
+	"testing"
+)
+
+func TestQueue(t *testing.T){
+	q := new(queue.Queue)
+	q.Push(1)
+	if q.Size() == 1{
+		t.Log("Size() ok", q.Size())
+	}else{
+		t.Error("Size() error", q.Size())
+	}
+
+	e := q.Top()
+	if e == 1 {
+		t.Log("Top() ok", e)
+	}else{
+		t.Error("Top() error")
+	}
+	q.Pop()
+	if q.Size() == 0 {
+		t.Log("Pop() ok")
+	}else{
+		t.Error("Pop() error")
+	}
+}
