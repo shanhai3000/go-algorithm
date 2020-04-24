e4d0e7ee7e1cae252a5624d3db5f36d2bfb41d99
shanhai3000 <shanhai3000@gmail.com>
2020-04-24 17:07:10 +0800

    symmetric test

---
+++ algorithm/symmetric_binarytree_test.go
@@ -0,0 +1,43 @@
+package algorithm
+
+import (
+	"algo/collection/node"
+	"testing"
+	"time"
+)
+
+/**
+ [1,2,2,3,4,4,3] symmetric。
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+*/
+
+func TestIsSymmetric1(t *testing.T) {
+	root := new(node.TreeNode)
+	root.Val = 1
+	root.Left = &node.TreeNode{Val: 2}
+	root.Right = &node.TreeNode{Val: 2}
+	root.Left.Left = &node.TreeNode{Val: 3}
+	root.Left.Right = &node.TreeNode{Val: 4}
+	root.Right.Left = &node.TreeNode{Val: 4}
+	root.Right.Right = nil
+
+	begin := time.Now()
+	ret := IsSymmetric0(root)
+	if ret {
+		t.Log("IsSymmetric0() Ok, use ", time.Since(begin))
+	} else {
+		t.Error("Error")
+	}
+
+	begin = time.Now()
+	ret = IsSymmetric1(root)
+	if ret {
+		t.Log("IsSymmetric1() Ok, use ", time.Since(begin))
+	} else {
+		t.Error("Error")
+	}
+}
