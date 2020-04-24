b376200f5f60540c654c2b3ce81a5f315b3a7603
shanhai3000 <shanhai3000@gmail.com>
2020-04-24 18:20:52 +0800

    traverse test

--- algorithm/traverse_tree_test.go
+++ algorithm/traverse_tree_test.go
@@ -1,1 +1,32 @@
 package algorithm
+
+import (
+	"algo/collection/node"
+	"fmt"
+	"testing"
+	"time"
+)
+
+/**
+    6
+   / \
+  4   8
+ / \ / \
+3  5 7  9
+*/
+func TestPreOrderTraverse0(t *testing.T) {
+	root := new(node.TreeNode)
+	root.Val = 6
+	root.Left = &node.TreeNode{Val: 4}
+	root.Right = &node.TreeNode{Val: 8}
+	root.Left.Left = &node.TreeNode{Val: 3}
+	root.Left.Right = &node.TreeNode{Val: 5}
+	root.Right.Left = &node.TreeNode{Val: 7}
+	root.Right.Right = &node.TreeNode{Val: 9}
+	begin := time.Now()
+	PreOrderTraverse0(root, PrintNode)
+	fmt.Println(time.Since(begin))
+	begin = time.Now()
+	PreOrderTraverse1(root, PrintNode)
+	fmt.Println(time.Since(begin))
+}
