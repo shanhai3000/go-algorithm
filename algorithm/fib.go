6736538927ae696995d55864f31a1ee2d091704a
shanhai3000 <shanhai3000@gmail.com>
2020-04-24 09:34:17 +0800

    iteration version of Fib func

--- algorithm/fib.go
+++ algorithm/fib.go
@@ -12,4 +12,29 @@ func Fib(n int) uint64{
 	}
 
 	return Fib(n-1) + Fib(n -2)
+}/* recursion version, bad */
+
+func Fib1(n int) uint64	{
+	arr := []uint64{0,1}
+	if n < 2{
+		return arr[n-1]
+	}
+	for i := 2; i < n; i++ {
+		arr[1] = arr[0] + arr[1]
+		arr[0] = arr[1] - arr[0]//two times operation, bad
+	}
+	return arr[1]
 }
+
+func Fib2(n int) uint64	{
+	arr := []uint64{0,1}
+	if n < 2{
+		return arr[n-1]
+	}
+	for i := 2; i < n; i++ {
+		t := arr[1]
+		arr[1] = arr[0] + arr[1]
+		arr[0] = t
+	}
+	return arr[1]
+}/*best version*/
---
+++ test/fib_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"algo/algorithm"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestFib1(t *testing.T) {
+	n := 200
+	begin := time.Now()
+	fmt.Println(algorithm.Fib1(n))
+	fmt.Println("Fib1 use ", time.Since(begin))//out 4.64µs
+	begin = time.Now()
+	fmt.Println(algorithm.Fib2(n))
+	fmt.Println("Fib2 use", time.Since(begin))//out  1.738µs
+}
