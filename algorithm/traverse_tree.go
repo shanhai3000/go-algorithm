f9d6fc1fa770af1e1d7935786f1a2861a5d730f1
shanhai3000 <shanhai3000@gmail.com>
2020-04-24 18:19:27 +0800

    add traverse visitor param

--- algorithm/traverse_tree.go
+++ algorithm/traverse_tree.go
@@ -2,14 +2,33 @@ package algorithm
 
 import (
 	"algo/collection/node"
+	"algo/collection/stack"
 	"fmt"
 )
+func PrintNode(node *node.TreeNode){
+	fmt.Println(node.Val)
+}
 
-func PreOrderTraverse0(root *node.TreeNode){
+func PreOrderTraverse0(root *node.TreeNode, visitor func(*node.TreeNode)){
 	// l-l-l-l-l-l-root-r
 	if root != nil {
-		fmt.Print(root.Val)
-		PreOrderTraverse0(root.Left)
-		PreOrderTraverse0(root.Right)
+		visitor(root)
+		PreOrderTraverse0(root.Left, visitor)
+		PreOrderTraverse0(root.Right, visitor)
 	}
 }/*recursion version*/
+
+func PreOrderTraverse1(root *node.TreeNode, visitor func(treeNode *node.TreeNode)) {
+	s := new(stack.Stack)
+	s.Push(root)
+	for ; !s.Empty(); {
+		t := s.Pop().(*node.TreeNode)
+		visitor(t)
+		if t.Right != nil{
+			s.Push(t.Right)
+		}
+		if t.Left != nil {
+			s.Push(t.Left)
+		}
+	}
+}
