fa5e6c2ada8e5bf9eb7226e56f62e624c5819028
shanhai3000 <shanhai3000@gmail.com>
2020-04-25 00:26:30 +0800

    test: HeapSort benchmark

--- test/algorithm/sort_test.go
+++ test/algorithm/sort_test.go
@@ -3,11 +3,30 @@ package algorithm
 import (
 	"algo/algorithm"
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 )
 
-func TestSort(t *testing.T) {
-	arr := []int{2, 1, 1, 5, 6, 3, 5}
+func TestQuickSort(t *testing.T) {
+	var arr []int
+	for i := 0; i < 100000000; i++ {
+		arr = append(arr, rand.Intn(1000))
+	}
+	begin := time.Now()
 	algorithm.QuickSort(arr)
-	fmt.Println(arr)
-}
+	use := time.Since(begin)
+	fmt.Println("use ", use)
+}/* Sort 100000000 elements using 5s */
+
+func TestHeapSort(t *testing.T) {
+	var arr []int
+	for i := 0; i < 100000000; i++ {
+		arr = append(arr, rand.Intn(1000))
+	}
+	begin := time.Now()
+	algorithm.HeapSort(arr)
+	use := time.Since(begin)
+	fmt.Println("use ", use)
+}/* Sort 100000000 elements using 31.8s */
+
