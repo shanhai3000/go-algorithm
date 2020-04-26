782e5987281555f1b2ec40e84b918b0e9d84b789
shanhai3000 <shanhai3000@gmail.com>
2020-04-26 23:27:40 +0800

    test: full permutation test

--- test/algorithm/array_permutation_test.go
+++ test/algorithm/array_permutation_test.go
@@ -1,1 +1,16 @@
 package algorithm
+
+import (
+	"algo/algorithm"
+	"algo/algorithm/util"
+	"testing"
+	"time"
+)
+
+func TestFullPermutation(t *testing.T) {
+	nums := []int{1, 2, 3, 4}
+	begin := time.Now()
+	res := algorithm.FullPermutation(nums)
+	util.DD(time.Since(begin))
+	util.DD(res)
+}
