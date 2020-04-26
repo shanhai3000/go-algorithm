82d593a57f1a65cc585d7bcad59fd9f0adedab75
shanhai3000 <shanhai3000@gmail.com>
2020-04-26 12:44:12 +0800

    test: max_string_scores test

--- test/algorithm/max_string_scores_test.go
+++ test/algorithm/max_string_scores_test.go
@@ -1,1 +1,12 @@
 package algorithm
+
+import (
+	"algo/algorithm"
+	"algo/algorithm/util"
+	"testing"
+)
+
+func TestMaxStringScores(t *testing.T){
+	s := "00111"
+	util.DD(algorithm.MaxStringScores(s))
+}
