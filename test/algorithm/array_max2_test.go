e614ac3167870428aecef02543151a1010ee0c8d
shanhai3000 <shanhai3000@gmail.com>
2020-04-26 13:47:18 +0800

    test: modify test case

--- test/algorithm/array_max2_test.go
+++ test/algorithm/array_max2_test.go
@@ -13,6 +13,8 @@ func TestArrayMax2(t *testing.T) {
 	for i := 0; i < 1000000; i ++{
 		arr = append(arr, rand.Intn(1000000))
 	}
+	arr = append(arr, 1000001)
+	arr = append(arr, 1000011)
 	begin := time.Now()
 	ret := algorithm.ArrayMax2(arr)
 	util.DD(time.Since(begin))
