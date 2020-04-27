05b4cb44acc533a5a57ca47c0de40bafa06d71c2
shanhai3000 <shanhai3000@gmail.com>
2020-04-27 16:44:22 +0800

    test: mv to test

--- test/algorithm/card_max_score_test.go
+++ test/algorithm/card_max_score_test.go
@@ -1,11 +1,12 @@
 package algorithm
 
 import (
+	"algo/algorithm"
 	"algo/algorithm/util"
 	"testing"
 )
 
 func TestCardMaxScore(t *testing.T) {
 	arr := []int{2,2,2}
-	util.DD(CardMaxScore(arr, 2))
+	util.DD(algorithm.CardMaxScore(arr, 2))
 }
