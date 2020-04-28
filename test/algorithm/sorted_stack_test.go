1f42ff29611013106a4b6116f480e367e8b5663f
shanhai3000 <shanhai3000@gmail.com>
2020-04-28 23:19:50 +0800

    test: add sorted stack test.

--- test/algorithm/sorted_stack_test.go
+++ test/algorithm/sorted_stack_test.go
@@ -1,45 +1,56 @@
 package algorithm
 
 import (
-	"algo/algorithm"
 	"algo/algorithm/util"
+	"algo/collection/sort"
+	"algo/collection/stack"
 	"testing"
 )
 
+type MyInt int
+
+func (i MyInt) CompareTo(t sort.Comparable) int {
+	if i > t.(MyInt) {
+		return 1
+	} else if i == t.(MyInt) {
+		return 0
+	} else {
+		return -1
+	}
+}
 func TestSortedStack(t *testing.T) {
-	s := new(algorithm.SortedStack)
+	s := new(stack.SortedStack)
+	util.DD(s.Peek())
 	s.Pop()
-	s.Push(40)
+	util.DD(s.Peek())
+	s.Push(MyInt(40))
+	util.DD(s.Peek())
 	s.Pop()
-	s.Push(19)
-	s.Push(44)
+	util.DD(s.Peek())
+	s.Push(MyInt(19))
+	util.DD(s.Peek())
+	s.Push(MyInt(44))
+	util.DD(s.Peek())
 	s.Pop()
+	util.DD(s.Peek())
 	s.Pop()
-	s.Push(42)
-	s.Push(8)
-	s.Push(29)
-	s.Push(25)
+	util.DD(s.Peek())
+	s.Push(MyInt(42))
+	util.DD(s.Peek())
+	s.Push(MyInt(8))
+	util.DD(s.Peek())
+	s.Push(MyInt(29))
+	util.DD(s.Peek())
+	s.Push(MyInt(25))
+	util.DD(s.Peek())
 	s.Pop()
+	util.DD(s.Peek())
 	s.Pop()
-	s.Push(52)
-	s.Push(63)
+	util.DD(s.Peek())
+	s.Push(MyInt(52))
+	util.DD(s.Peek())
+	s.Push(MyInt(63))
 	s.Pop()
-	s.Push(47)
-	s.Pop()
-	s.Push(45)
-	s.Push(52)
-	s.Pop()
-	s.Pop()
-	s.Push(17)
-	s.Pop()
-	s.Push(6)
-	s.Push(30)
-	s.Push(51)
-	s.Push(46)
-	s.Push(2)
-	s.Push(56)
-	s.Push(39)
-	s.Push(38)
 
 	util.DD(s.Peek())
 
