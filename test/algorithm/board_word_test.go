1c1d03c75b4d852dd5441236acb87d429b72659a
shanhai3000 <shanhai3000@gmail.com>
2020-04-30 10:27:33 +0800

    test: board word search test.

--- test/algorithm/board_word_test.go
+++ test/algorithm/board_word_test.go
@@ -1,1 +1,18 @@
 package algorithm
+
+import (
+	"algo/algorithm"
+	"algo/algorithm/util"
+	"testing"
+)
+
+func TestBoardWord(t *testing.T) {
+	board := [][]byte{
+		{'A', 'B', 'C', 'E'},
+		{'S', 'F', 'C', 'S'},
+		{'A', 'D', 'E', 'E'},
+	}
+	word := "ABS"
+	res := algorithm.WordExist(board, word)
+	util.DD(res)
+}
