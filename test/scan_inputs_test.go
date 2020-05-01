547ba1d8f1573ffc08489c8d383f207ab2d9e77f
shanhai3000 <shanhai3000@gmail.com>
2020-05-01 23:56:27 +0800

    test: scan input

--- test/scan_inputs_test.go
+++ test/scan_inputs_test.go
@@ -1,1 +1,19 @@
 package test
+
+import (
+	"algo/algorithm/util"
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestScanInputs(t *testing.T){
+	in := bufio.NewReader(os.Stdin)
+	for i := 0; ;i++{
+		line,err := in.ReadSlice('\n')
+		if line[0] == '\n' ||  err != nil{
+			break
+		}
+		util.DD(line)
+	}
+}
